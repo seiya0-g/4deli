d2fb094542d7ba5b1ae3b745e9dc0ac26eef30a1
Seiya <34178299+seiya0-g@users.noreply.github.com>
2026-06-13 09:23:45 +0900

    Copy to docs/sites/2026-06-13

---
+++ docs/sites/2026-06-13/js/base/s0.js
@@ -0,0 +1,41 @@
+/**
+ * s0.js v0.9.0
+ * © 2025 seiya0-g
+ * license: All Rights Reserved
+ * source: https://github.com/seiya0-g
+ */
+'use strict';
+(function () {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+  function init() {
+
+    document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const header = document.querySelector('header');
+        const headerHeight = header ? header.offsetHeight : 0;
+        const targetId = this.getAttribute('href');
+        const target = document.querySelector(targetId);
+        if (target) {
+          const rect = target.getBoundingClientRect();
+          const offsetTop = window.pageYOffset + rect.top - headerHeight;
+
+          window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+          });
+
+          if (offsetTop <= 0) {
+            history.pushState(null, '', window.location.pathname);
+          } else {
+            history.pushState(null, '', targetId);
+          }
+        }
+      });
+    });
+  }
+})();
